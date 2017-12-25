17a96e1ba5b71994b07a5ce01cf9d8df10eb0e58
kilho <kim.kilho@sualab.com>
2017-12-25 16:21:49 +0900

    Add AccuracyEvaluator in learning.evaluators

--- learning/evaluators.py
+++ learning/evaluators.py
@@ -1,7 +1,10 @@
+import os
+import pickle as pkl
 from abc import abstractmethod, abstractproperty
-import tensorflow as tf
+
 import numpy as np
-import time
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
 
 
 class Evaluator(object):
@@ -25,7 +28,7 @@ class Evaluator(object):
         pass
 
     @abstractmethod
-    def is_better(self, curr, best):
+    def is_better(self, curr, best, **kwargs):
         """
         Function to return whether current performance score is better than current best.
         This should be implemented.
@@ -33,3 +36,59 @@ class Evaluator(object):
         :param best: Float, current best performance.
         """
         pass
+
+
+class AccuracyEvaluator(Evaluator):
+    """Evaluator with accuracy metric."""
+
+    def worst_score(self):
+        """The worst performance score."""
+        return 0.0
+
+    def score(self, y_true, y_pred):
+        """Compute accuracy for a given prediction."""
+        return accuracy_score(y_true.argmax(axis=1), y_pred.argmax(axis=1))
+
+    def is_better(self, curr, best, **kwargs):
+        """
+        Return whether current performance score is better than current best,
+        with consideration of the relative threshold to the given performance score.
+        """
+        score_threshold = kwargs.pop('score_threshold', 1e-4)
+        relative_eps = 1.0 + score_threshold
+        return lambda curr, best: curr > best * relative_eps
+
+    def plot_learning_curve(self, exp_idx, step_losses, step_scores, eval_scores=None,
+                            l_rate_decay_iters_list=None,
+                            plot=False, save=True, img_dir='.'):
+        fig, axes = plt.subplots(2, 1, figsize=(10, 10))
+        axes[0].plot(np.arange(1, len(step_losses)+1), step_losses, marker='')
+        axes[0].set_ylabel('loss')
+        axes[0].set_xlabel('Number of iterations')
+        axes[1].plot(np.arange(1, len(step_scores)+1), step_scores, color='b', marker='')
+        if eval_scores is not None:
+            axes[1].plot(np.arange(1, len(eval_scores)+1), eval_scores, color='r', marker='')
+        axes[1].set_ylim(0.5, 1.0)  # FIXME
+        axes[1].set_ylabel('Accuracy')
+        axes[1].set_xlabel('Number of epochs')
+
+        # Show learning rate decay points on learning curve plots
+        if l_rate_decay_iters_list is not None:
+            for it in l_rate_decay_iters_list:
+                axes[0].axvline(x=it, color='k', linestyle='--', linewidth=1)
+                axes[1].axvline(x=it / 100, color='k', linestyle='--', linewidth=1)
+
+        if save:
+            plot_img_filename = 'learning_curve-result{}.svg'.format(exp_idx)
+            if not os.path.exists(img_dir):
+                os.makedirs(img_dir)
+            fig.savefig(os.path.join(img_dir, plot_img_filename))
+
+            pkl_filename = 'learning_curve-result{}.pkl'.format(exp_idx)
+            with open(os.path.join(img_dir, pkl_filename), 'wb') as fo:
+                pkl.dump([step_losses, step_scores, eval_scores], fo)
+
+        if plot:
+            plt.show()
+        else:
+            plt.close()
