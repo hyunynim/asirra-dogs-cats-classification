e9b73e28982d8508f115ca6093a0563e92550ded
kilho <kim.kilho@sualab.com>
2017-12-25 12:52:51 +0900

    Redefine Evaluator class, along with its optimizers in learning subpackage

--- learning/evaluators.py
+++ learning/evaluators.py
@@ -1,4 +1,4 @@
-from abc import abstractmethod
+from abc import abstractmethod, abstractproperty
 import tensorflow as tf
 import numpy as np
 import time
@@ -9,28 +9,13 @@ class Evaluator(object):
     Base class for evaluation functions.
     """
 
-    def __init__(self, sess, model, eval_set, **kwargs):
-        """
-        Evaluator initializer.
-        :param sess: tf.Session.
-        :param model: Model to be evaluated.
-        :param eval_set: DataSet.
-        """
-        self.sess = sess
-        self.model = model
-        self.eval_set = eval_set
-
-        self.batch_size = kwargs.pop('batch_size', 256)
-
-    @abstractmethod
-    def worst_performance(self):
-        """
-        The worst performance score.
-        """
+    @abstractproperty
+    def worst_score(self):
+        """The worst performance score."""
         pass
 
     @abstractmethod
-    def performance(self, y_true, y_pred):
+    def score(self, y_true, y_pred):
         """
         Performance metric for a given prediction.
         This should be implemented.
@@ -42,48 +27,9 @@ class Evaluator(object):
     @abstractmethod
     def is_better(self, curr, best):
         """
-        Function to return whether current performance is better than current best.
+        Function to return whether current performance score is better than current best.
         This should be implemented.
         :param curr: Float, currently given performance.
         :param best: Float, current best performance.
         """
         pass
-
-    def eval(self, details=False, verbose=False):
-        """Evaluate the model."""
-        eval_results = dict()    # dictionary to contain evaluation results and details
-        eval_size = self.eval_set.num_examples
-        num_steps = eval_size // self.batch_size
-
-        if verbose:
-            print('Running evaluation loop...')
-
-        # Evaluation loop
-        eval_y_true, eval_y_pred = [], []
-        start_time = time.time()
-        for i in range(num_steps+1):
-            if i == num_steps:
-                _batch_size = eval_size - num_steps*self.batch_size
-            else:
-                _batch_size = self.batch_size
-            X, y_true = self.eval_set.next_batch(_batch_size, shuffle=False, augment=False)
-
-            # Compute predictions
-            y_pred = self.sess.run(self.model.predict,
-                                   feed_dict={self.model.X: X,
-                                              self.model.is_train: False})
-            eval_y_true.append(y_true)
-            eval_y_pred.append(y_pred)
-        if verbose:
-            print('Total evaluation time(sec): {}'.format(time.time() - start_time))
-
-        eval_y_true = np.concatenate(eval_y_true, axis=0)    # (N, 2)
-        eval_y_pred = np.concatenate(eval_y_pred, axis=0)    # (N, 2)
-
-        # Store evaluation results in a dictionary
-        if details:
-            eval_results['y_true'] = eval_y_true
-            eval_results['y_pred'] = eval_y_pred
-        eval_results['performance'] = self.performance(eval_y_true, eval_y_pred)
-        return eval_results
-
--- learning/optimizers.py
+++ learning/optimizers.py
@@ -6,7 +6,7 @@ import time
 
 class Optimizer(object):
     """
-    Base class for optimization functions.
+    Base class for gradient-based optimization functions.
     """
 
     def __init__(self, sess, model, train_set, evaluator, **kwargs):
@@ -15,6 +15,7 @@ class Optimizer(object):
         :param sess: tf.Session.
         :param model: Model to be learned.
         :param train_set: DataSet.
+        :param evaluator: Evaluator.
         """
         self.sess = sess
         self.model = model
@@ -25,19 +26,16 @@ class Optimizer(object):
         self.num_epochs = kwargs.pop('num_epochs', 320)
         self.init_learning_rate = kwargs.pop('learning_rate', 0.01)
 
-        self.learning_rate = tf.placeholder(tf.float32)
+        self.learning_rate_placeholder = tf.placeholder(tf.float32)
+        self.optimize = self._optimize_op()
+        self.saver = tf.train.Saver()
 
     def _reset(self):
         """Reset some variables."""
         self.curr_epoch = 1
-        self.best_performance = self.evaluator.worst_performance()
-        self.saver = tf.train.Saver()
-
+        self.best_score = self.evaluator.worst_score
         self.curr_learning_rate = self.init_learning_rate    # current learning rate
-        self.optimize = self._optimize_op()
-
-        # Initialize all weights
-        self.sess.run(tf.global_variables_initializer())
+        self.sess.run(tf.global_variables_initializer())    # initialize all weights
 
     @abstractmethod
     def _optimize_op(self):
@@ -61,26 +59,63 @@ class Optimizer(object):
             self.sess.run([self.optimize, self.model.loss, self.model.predict],
                           feed_dict={self.model.X: X, self.model.y: y_true,
                                      self.model.is_train: True,
-                                     self.learning_rate: self.curr_learning_rate})
+                                     self.learning_rate_placeholder: self.curr_learning_rate})
         step_results['loss'] = loss
         step_results['y_true'] = y_true
         step_results['y_pred'] = y_pred
 
         return step_results
 
-    @abstractmethod
-    def _update_learning_rate(self):
+    def predict(self, dataset, verbose=False):
         """
-        Update current learning rate, by its own schedule.
-        This should be implemented, and should not be called manually.
+        Make predictions for the given dataset.
+        :param dataset: DataSet.
+        :param verbose: Boolean, whether to print details during prediction.
         """
+        batch_size = self.batch_size
 
-    def train(self, details=False, eval=True, verbose=True):
+        pred_results = dict()
+        pred_size = dataset.num_examples
+        num_steps = pred_size // batch_size
+
+        if verbose:
+            print('Running prediction loop...')
+
+        # Evaluation loop
+        _y_true, _y_pred = [], []
+        start_time = time.time()
+        for i in range(num_steps+1):
+            if i == num_steps:
+                _batch_size = pred_size - num_steps*batch_size
+            else:
+                _batch_size = batch_size
+            X, y_true = dataset.next_batch(_batch_size, shuffle=False, augment=False)
+
+            # Compute predictions
+            y_pred = self.sess.run(self.model.predict,
+                                   feed_dict={self.model.X: X,
+                                              self.model.is_train: False})
+            _y_true.append(y_true)
+            _y_pred.append(y_pred)
+        if verbose:
+            print('Total evaluation time(sec): {}'.format(time.time() - start_time))
+
+        _y_true = np.concatenate(_y_true, axis=0)    # (N, 2)
+        _y_pred = np.concatenate(_y_pred, axis=0)    # (N, 2)
+        pred_results['y_true'] = _y_true
+        pred_results['y_pred'] = _y_pred
+
+        return pred_results
+
+    def train(self, eval_set=None, details=False, verbose=True):
         """
         Run optimizer to train the model.
-        :param eval: Boolean, whether to perform evaluation on every epoch.
+        :param eval_set: Evaluation set, performs evaluation with it on every epoch if it is not None,
+                         while performs evaluation on training set instead if it is None.
+        :param details: Boolean, whether to return detailed results.
+        :param verbose: Boolean, whether to print details during training.
         """
-        train_results = dict()    # dictionary to contain training results and details
+        train_results = dict()    # dictionary to contain training(, evaluation) results and details
         train_size = self.train_set.num_examples
         num_steps_per_epoch = train_size // self.batch_size
         num_steps = self.num_epochs * num_steps_per_epoch
@@ -89,7 +124,7 @@ class Optimizer(object):
             print('Running training loop...')
             print('Number of training iterations: {}'.format(num_steps))
 
-        step_losses, step_performances, eval_performances = [], [], []
+        step_losses, step_scores, eval_scores = [], [], []
         start_time = time.time()
         for i in range(num_steps):
             step_results = self._step()
@@ -97,68 +132,48 @@ class Optimizer(object):
             step_losses.append(step_loss)
 
             if (i+1) % num_steps_per_epoch == 0:
-                step_performance = self.evaluator.performance(step_results['y_true'], step_results['y_pred'])
-                step_performances.append(step_performance)
-                if eval:
+                step_score = self.evaluator.score(step_results['y_true'], step_results['y_pred'])
+                step_scores.append(step_score)
+                if eval_set is not None:
                     # Evaluate current model
-                    eval_performance = self.evaluator.eval()['performance']
-                    eval_performances.append(eval_performance)
+                    eval_results = self.predict(eval_set, verbose=False)
+                    eval_score = self.evaluator.score(eval_results['y_true'], eval_results['y_pred'])
+                    eval_scores.append(eval_score)
 
                     if verbose:
                         print('[epoch {}]\tloss: {} |Train performance: {} |Eval performance: {} |learning_rate: {}'\
-                              .format(self.curr_epoch, step_loss,
-                                      step_performance, eval_performance, self.curr_learning_rate))
+                              .format(self.curr_epoch, step_loss, step_score, eval_score, self.curr_learning_rate))
 
-                    # Keep track of the current best model for evaluation set
-                    if self.evaluator.is_better(eval_performance, self.best_performance):
-                        self.best_performance = eval_performance
+                    # Keep track of the current best model for validation set
+                    if self.evaluator.is_better(eval_score, self.best_score):
+                        self.best_score = eval_score
                         self.saver.save(self.sess, '/tmp/model.ckpt')    # save current weights
 
                 else:
                     if verbose:
                         print('[epoch {}]\tloss: {} |Train performance: {} |learning_rate: {}'\
-                              .format(self.curr_epoch, step_loss,
-                                      step_performance, self.curr_learning_rate))
+                              .format(self.curr_epoch, step_loss, step_score, self.curr_learning_rate))
 
                     # Keep track of the current best model for training set
-                    if self.evaluator.is_better(step_performance, self.best_performance):
-                        self.best_performance = step_performance
+                    if self.evaluator.is_better(step_score, self.best_score):
+                        self.best_score = step_score
                         self.saver.save(self.sess, '/tmp/model.ckpt')    # save current weights
 
-                self._update_learning_rate()
                 self.curr_epoch += 1
 
         if verbose:
             print('Total training time(sec): {}'.format(time.time() - start_time))
-            print('Best {} performance: {}'.format('evaluation' if eval else 'training',
-                                                   self.best_performance))
+            print('Best {} score: {}'.format('evaluation' if eval else 'training',
+                                             self.best_score))
 
         if details:
             # Store training results in a dictionary
             train_results['step_losses'] = step_losses    # (num_iterations)
-            train_results['step_performances'] = step_performances    # (num_epochs)
-            if eval:
-                train_results['eval_performances'] = eval_performances    # (num_epochs)
+            train_results['step_scores'] = step_scores    # (num_epochs)
+            if eval_set is not None:
+                train_results['eval_scores'] = eval_scores    # (num_epochs)
 
             return details
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print('Done.')
 
