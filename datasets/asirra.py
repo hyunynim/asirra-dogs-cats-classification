723e7455ce3cc6413bc1f47d3ae1ce53e45a80de
kilho <kim.kilho@sualab.com>
2017-12-26 13:35:30 +0900

    Fix read_asirra_subset function to be memory-efficient, in datasets.asirra

--- datasets/asirra.py
+++ datasets/asirra.py
@@ -14,7 +14,6 @@ def read_asirra_subset(subset_dir, one_hot=True, sample_size=None):
     :return: X_set, y_set
     """
     # Read trainval data
-    X_set, y_set = [], []
     filename_list = os.listdir(subset_dir)
     set_size = len(filename_list)
 
@@ -22,6 +21,9 @@ def read_asirra_subset(subset_dir, one_hot=True, sample_size=None):
         filename_list = np.random.choice(filename_list, size=sample_size, replace=False)
         set_size = sample_size
 
+    # Pre-allocate data arrays
+    X_set = np.empty((set_size, 256, 256, 3), dtype=np.float32)    # (N, H, W, 3)
+    y_set = np.empty((set_size), dtype=np.uint8)                   # (N,)
     for i, filename in enumerate(filename_list):
         if i % 1000 == 0:
             print('Reading subset data: {}/{}...'.format(i, set_size), end='\r')
@@ -33,17 +35,13 @@ def read_asirra_subset(subset_dir, one_hot=True, sample_size=None):
         file_path = os.path.join(subset_dir, filename)
         img = imread(file_path)
         # shape: (H, W, 3), range: [0, 255]
-        img_resized = resize(img, (256, 256), mode='constant').astype(np.float32)
+        img = resize(img, (256, 256), mode='constant').astype(np.float32)
         # shape: (256, 256, 3), range: [0.0, 1.0]
-        X_set.append(img_resized)
-        y_set.append(y)
-
-    # Stack all subset data
-    X_set = np.stack(X_set)  # shape: (N, 256, 256, 3)
-    y_set = np.stack(y_set).astype(np.uint8)  # shape: (N,)
+        X_set[i] = img
+        y_set[i] = y
 
     if one_hot:
-        # Convert labels to one-hot vectors
+        # Convert labels to one-hot vectors: (N, num_classes)
         y_set_oh = np.zeros((set_size, 2), dtype=np.uint8)
         y_set_oh[np.arange(set_size), y_set] = 1
         y_set = y_set_oh
